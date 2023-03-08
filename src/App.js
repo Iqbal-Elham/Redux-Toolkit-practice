dff1703caf48c5c1479208949bbf31d00e95a910
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-08 11:35:30 +0430

    Add Provider and store in app.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.css';
+import CartContainer from './components/CartContainer';
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <CartContainer />
     </div>
   );
 }
