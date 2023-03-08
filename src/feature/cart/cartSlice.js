1c070949ea6016ac7af95ee59e9257e3e5738e5d
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-08 18:04:52 +0430

    Update the items with increase, decrease and remove functionality

--- src/feature/cart/cartSlice.js
+++ src/feature/cart/cartSlice.js
@@ -14,10 +14,22 @@ export const cartSlice = createSlice({
     reducers: {
         clearAll: (state) => {
             state.cartItems = []
-        }
+        },
+        removeItem: (state, action) => {
+            state.cartItems = state.cartItems.filter((item) => item.id !== action.payload);
+        },
+        increase: (state, { payload }) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload);
+            cartItem.amount += 1;
+        }, 
+        decrease: (state, { payload }) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload);
+            cartItem.amount -= 1;
+        },
+        
     }
 })
 
 
-export const { clearAll } = cartSlice.actions; 
+export const { clearAll, removeItem, increase, decrease } = cartSlice.actions; 
 export default cartSlice.reducer;
