c2bcf0975d15563bd0c796ff6c8f9d4416f98307
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-09 13:46:40 +0430

    Add useEffect for the loading

--- src/App.js
+++ src/App.js
@@ -2,20 +2,38 @@ import './App.css';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
 import CartContainer from './components/CartContainer';
-import { calculateTotal } from './feature/cart/cartSlice';
+import { calculateTotal, getCartItems } from './feature/cart/cartSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import Model from './components/Model';
+
 
 
 function App() {
-  const { cartItems } = useSelector(store => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.model);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(calculateTotal());
-  }, [cartItems]);
+    dispatch(calculateTotal())
+    // eslint-disable-next-line
+  }, [ cartItems ]);
+
+  useEffect(() => {
+    dispatch(getCartItems())
+    // eslint-disable-next-line
+  }, []);
+
+  if(isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
 
   return (
     <div className="App">
+      { isOpen && <Model />}
      <Navbar />
      <CartContainer />
     </div>
